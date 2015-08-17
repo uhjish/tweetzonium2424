d520f75a81b6699fad2ec6dbeaf409ae55b341fc
Ajish George <ajish@rootedinsights.com>
2015-08-16 22:37:39 -0400

    add a counting words example

--- twtools.py
+++ twtools.py
@@ -1,5 +1,6 @@
 import tweepy
 import pandas as pd
+from collections import Counter
 from ConfigParser import ConfigParser
 
 class LocationWOE:
@@ -11,8 +12,6 @@ config = ConfigParser()
 config.read("credentials")
 consumer_token = config.get("twitter","consumer_token") 
 consumer_secret = config.get("twitter","consumer_secret")
-#access_token = config.get("twitter","access_token")
-#access_secret = config.get("twitter","access_secret")
 
 #api-key authentication
 auth = tweepy.OAuthHandler( consumer_token, consumer_secret )
@@ -22,10 +21,14 @@ auth = tweepy.OAuthHandler( consumer_token, consumer_secret )
 twapi = tweepy.API(auth)
 
 #cursor based search
+#search can take result type "recent", "popular", by default is "mixed"
 cxr = tweepy.Cursor(twapi.search, q="Bob Dole", result_type="recent").items(10)
 
-cts = pd.DataFrame( [ (x.author.screen_name, x.author.followers_count, x.author.friends_count) for x in cxr] )
-cts = cts.drop_duplicates()
+cts = pd.DataFrame( [ (x.text, x.author.screen_name, x.author.followers_count, x.author.friends_count) for x in cxr] )
+
+bag_of_words = " ".join(cts.ix[:,0].values.tolist())
+
+print pd.DataFrame(Counter(bag_of_words.split()).most_common()[:10])
 
 print cts
 
